f589da9cf09fc30046af171bf0536d79e8849ced
Oscar Muñoz <oscar@b2chat.io>
2023-05-01 14:55:48 -0500

    InsomnioDev Child Theme Ver 1.0.0
    Child Theme stable version

---
+++ assets/scripts/header-scripts.js
@@ -0,0 +1,3 @@
+/**
+ * Main child theme header scripts
+ */
---
+++ includes/shortcodes/child_shortcodes.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('ABSPATH')) exit; //Exit if accessed directly.
+
+/**
+ * Main child theme shortcodes class
+ * @version 0.0.1
+ */
+class IdtChildShortcodes
+{
+    /**
+     * Child shortcode code
+     */
+}
